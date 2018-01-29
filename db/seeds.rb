a877d5dc33d1316dec4cd217265c6a6f2b21ee4c
Jennifer Driskell <jennifer@compose.io>
2018-01-29 15:27:27 -0600

    Initial commit of seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.destroy_all
+ 
+def self.short_code
+  source = [*?A..?Z] - ['O']
+  short = ''
+  5.times { short << source.sample.to_s }
+  short
+end
+
+Game.create!(
+  name: short_code
+  ).players.create!([
+    {
+      name: "Josh",
+      game_id: short_code,
+      admin: true,
+      score: 5,
+      duration: 150,
+      team: 0
+    },
+    {
+      name: "Amy",
+      game_id: short_code,
+      admin: false,
+      score: 7,
+      duration: 160,
+      team: 1
+    },
+    {
+      name: "David",
+      game_id: short_code,
+      admin: false,
+      score: 10,
+      duration: 120,
+      team: 0
+    },
+    {
+      name: "Louis",
+      game_id: short_code,
+      admin: false,
+      score: 6,
+      duration: 120,
+      team: 1
+    }
+  ])
+ 
+p "Created #{Game.count} games"
