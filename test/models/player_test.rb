9bcf68873898406ff1415253e6e0a6aa30d158be
Brandon Mathis <brandon@imathis.com>
2018-01-30 15:10:18 -0600

    Moved player admin assignment and team assignment to Player model

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -5,4 +5,21 @@ class PlayerTest < ActiveSupport::TestCase
     assert Game.first.players.create!({ name: "Bob" })
     assert_equal Game.first.players.last.name, "Bob"
   end
+
+  test "should make first player an admin" do
+    game = Game.create!(name: 'TESTGAME')
+    game.players.create!(name: 'AdminPlayer')
+    assert game.players.first.admin
+  end
+
+  test "should choose teams for players" do
+    game = Game.create!(name: 'TESTGAME2')
+    game.players.create!(name: 'test player 1')
+    game.players.create!(name: 'test player 2')
+    game.players.create!(name: 'test player 3')
+
+    assert_equal game.players[0].team, 0
+    assert_equal game.players[1].team, 1
+    assert_equal game.players[2].team, 0
+  end
 end
