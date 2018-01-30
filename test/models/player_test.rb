0c902a37dbed40edb84eff8ce2be3f93925a04d1
Brandon Mathis <brandon@imathis.com>
2018-01-30 13:47:12 -0600

    More testing on game, added player tests

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should add a player" do
+    assert Game.first.players.create!({ name: "Bob" })
+    assert_equal Game.first.players.last.name, "Bob"
+  end
 end
